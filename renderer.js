dd8d932255b8e405988e4dbbb91e44298d7bf9db
yura <yura_b@me.com>
2020-04-01 13:59:52 +0300

    test

--- renderer.js
+++ renderer.js
@@ -4,3 +4,9 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const {ipcRenderer, shell} = require('electron');
+
+
+ipcRenderer.on('upd-message', function (e, text) {
+    console.log('Message from updater: ', text);
+});
